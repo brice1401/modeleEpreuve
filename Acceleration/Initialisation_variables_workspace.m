57da56f8556dd1c799dff9dcefa5f5cfc5d83066
brice1401 <brice.roblot@outlook.com>
2017-03-04 20:09:32 +0100

    ajout la nouvelle équation mécanique avec l'application du TEC au véhicule

--- Acceleration/Initialisation_variables_workspace.m
+++ Acceleration/Initialisation_variables_workspace.m
@@ -18,18 +18,21 @@ m = mtot;
 g = 9.81;
 h = (mpilote*h_p + mveh*h_v)/m; %hauteur du centre de gravité
 D_roue = 0.53; % Diametre exterieur de la roue (m)
+R = D_roue/2; %Rayon de la roue
 
-coeff_roul = 0.01; % Coefficient de resistance au roulement du pneu
-b = coeff_roul*2/D_roue; % Decalage du point d'appui (m)
-c_roul = m*g*b; % Resistance au roulement (N.m)
+coeff_roul = 0.01; % Coefficient de resistance au roulement du pneu (delta en m)
 
-J_rot = 0; % Inertie equivalente des masses en rotation (kg.m²)
-J_trans = m*D_roue*D_roue/4; % Inertie equivalente des masses en translation (kg.m²)
-J_eq = J_trans + J_rot; % Inertie totale (kg.m²)
+J_rot = 0.111; % Inertie d'une roue équipée (kg.m²)
+J_trans = m*R^2; % Inertie equivalente des masses en translation (kg.m²)
+J_eq = J_trans + 4*J_rot; % Inertie totale (kg.m²)
 
-c_moteur = 60;  % Couple délivré par le moteur
+%Moteur
+%Courbe moteur
+C_mot = [0,45.126,51.01200000000001,52.974000000000004,51.01200000000001,48.069,49.050000000000004,51.01200000000001,56.898,58.86,58.86,57.879000000000005,56.898,54.936,51.993,48.069,44.145,40.221,36.297000000000004,32.373];
 
-parametreGeo = [h, e, b, v];
+%Régime correspondant
+regime = [0, 4500:500:13500];
+
 
 %Paramètre boite de vitesse :
 rap_pri = 36/76;
@@ -43,16 +46,17 @@ rap_couronne = 13/45;
 
 %Parametre aerodynamique
 rho = 1.2; %masse volumique de l'air en kg/m3
-Cx = 0.3; %coeff de trainée
+Cx = 0.7; %coeff de trainée
+S = 1; %surface de référence (m2)
 
 %Paramètre initiaux de la simulation
 vitesse_initiale = 0;
 
 
 %Lancement de la simulation :
-sim('Accel_model_test');
+%sim('Accel_model_test');
 
 
 %Création des graphiques
 
-run('DessinCourbe.m');
+%run('DessinCourbe.m');
